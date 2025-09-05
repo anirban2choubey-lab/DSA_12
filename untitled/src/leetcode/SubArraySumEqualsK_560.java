bceafae7f16ac64bdec80daad8a29d223b90ef3f
Anirban <anirbandreamsalot@gmail.com>
2025-09-05 19:31:18 +0530

    SubArraySumEqualsK_560, trap rain water 42

--- untitled/src/leetcode/SubArraySumEqualsK_560.java
+++ untitled/src/leetcode/SubArraySumEqualsK_560.java
@@ -4,10 +4,10 @@ import java.util.HashMap;
 
 public class SubArraySumEqualsK_560 {
     public static void main(String[] args) {
-        int[] nums = {-1, -1, 1};//
-        System.out.println(subarraySum(nums, 0));
+        int[] nums = {1, 2, 3};//
+        System.out.println(subarraySum(nums, 3));
     }
-
+//explanation : https://www.youtube.com/watch?v=fFVZt-6sgyo
     //the confusing part is why (preSum-k) is working but k-presum is not working
     public static int subarraySum(int[] nums, int k) {
         int res = 0;
