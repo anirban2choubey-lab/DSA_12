6e64017c3dd6bf16bc6e1804eab9430c22b61c50
Anirban <anirbandreamsalot@gmail.com>
2026-01-17 09:38:43 +0530

    Queue

--- untitled/src/leetcode/DailyTempareture.java
+++ untitled/src/leetcode/DailyTempareture.java
@@ -6,7 +6,7 @@ import java.util.Stack;
 public class DailyTempareture {
     public static void main(String[] args) {
         int[] temps = {73, 74, 75, 71, 69, 72, 76, 73};
-//[1,1,4,2,1,1,0,0]
+      //[1,1,4,2,1,1,0,0]
         System.out.println(Arrays.toString(dailyTemperatures(temps)));
     }
 
